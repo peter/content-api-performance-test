3b8383476c505522084624a2a49f3b0c6b7e07f1
Peter Marklund <peter@marklunds.com>
2025-07-29 14:38:19 +0200

    chore: add supabase support to run.js script

--- scripts/performance-test/run.js
+++ scripts/performance-test/run.js
@@ -3,12 +3,15 @@
 import _ from 'lodash';
 import axios from 'axios';
 import assert from 'node:assert';
+import { ulid } from 'ulid';
 
 const BASE_URL = process.env.BASE_URL || 'http://localhost:8888';
 const TEST_LIMIT = parseInt(process.env.TEST_LIMIT || 10_000)
 const TEST_PARALLEL = parseInt(process.env.TEST_PARALLEL || 100)
 const TEST_DATA_FIELD = process.env.TEST_DATA_FIELD !== 'false'
 const TEST_CREATE_ONLY = process.env.TEST_CREATE_ONLY === 'true'
+const TEST_SUPABASE = process.env.TEST_SUPABASE === 'true'
+const TEST_HEADERS = process.env.TEST_HEADERS ? JSON.parse(process.env.TEST_HEADERS) : undefined
 
 const LOG_LEVELS = ['DEBUG', 'INFO', 'ERROR']
 const LOG_LEVEL = (process.env.LOG_LEVEL || 'DEBUG').toUpperCase()
@@ -62,12 +65,44 @@ function assertRecentDate(date) {
     }
 }
 
+async function getContent(id, options = {}) {
+    if (TEST_SUPABASE) {
+        const response = await axios.get(`${BASE_URL}/content?id=eq.${id}`, options)
+        return { headers: response.headers, data: response.data?.[0] }
+    } else {
+        const response = await axios.get(`${BASE_URL}/content/${id}`, options)
+        return { headers: response.headers, data: response.data }
+    }
+}
+
+async function updateContent(id, content, options = {}) {
+    if (TEST_SUPABASE) {
+        const response = await axios.patch(`${BASE_URL}/content?id=eq.${id}`, content, options)
+        return { headers: response.headers, data: response.data?.[0] }
+    } else {
+        const response = await axios.put(`${BASE_URL}/content/${id}`, content, options)
+        return { headers: response.headers, data: response.data }
+    }
+}
+
+async function deleteContent(id, options = {}) {
+    if (TEST_SUPABASE) {
+        const response = await axios.delete(`${BASE_URL}/content?id=eq.${id}`, options)
+        return { headers: response.headers, data: response.data?.[0] }
+    } else {
+        const response = await axios.delete(`${BASE_URL}/content/${id}`, options)
+        return { headers: response.headers, data: response.data }
+    }
+}
+
 async function runTest(batchIndex, index) {
     const runId = `${batchIndex}-${index}-${Math.random().toString(36).substring(2, 15)}`
     const testStartTime = Date.now()
     const requestElapsed = { create: [], read: [], update: [], delete: [] }
+    const serverElapsed = { create: [], read: [], update: [], delete: [] }
     const createdAt = new Date().toISOString()
     const content = {
+        id: ulid(),
 		title:  `Smoke Test Content ${runId}`,
 		body:   `This is smoke test content number ${runId}`,
 		author: "Smoke Tester",
@@ -80,17 +115,25 @@ async function runTest(batchIndex, index) {
 		}
     }
 
+    const headers = TEST_HEADERS || {}
+
     // CREATE
     let startTime = Date.now()
-    let response = await axios.post(`${BASE_URL}/content`, content)
+    let response = await axios.post(`${BASE_URL}/content`, content, { headers })
     requestElapsed.create.push(Date.now() - startTime)
-    const id = response.data.id
+    if (response.headers['x-response-time']) {
+        serverElapsed.create.push(Number(response.headers['x-response-time']))
+    }
+    const id = response.data.id || content.id
 
     if (!TEST_CREATE_ONLY) {
         // READ
         startTime = Date.now()
-        response = await axios.get(`${BASE_URL}/content/${id}`)
+        response = await getContent(id, { headers })
         requestElapsed.read.push(Date.now() - startTime)
+        if (response.headers['x-response-time']) {
+            serverElapsed.read.push(Number(response.headers['x-response-time']))
+        }
         assert.strictEqual(response.data.id, id)
         assert.strictEqual(response.data.title, content.title)
         assert.strictEqual(response.data.body, content.body)
@@ -104,34 +147,52 @@ async function runTest(batchIndex, index) {
 
         // UPDATE
         startTime = Date.now()
-        await axios.put(`${BASE_URL}/content/${id}`, {
+        await updateContent(id, {
             ...content,
             status: "published",
             title: `${content.title} (updated)`,
-        })
+        }, { headers })
         requestElapsed.update.push(Date.now() - startTime)
+        if (response.headers['x-response-time']) {
+            serverElapsed.update.push(Number(response.headers['x-response-time']))
+        }
 
         // READ
         startTime = Date.now()
-        response = await axios.get(`${BASE_URL}/content/${id}`)
+        response = await getContent(id, { headers })
         requestElapsed.read.push(Date.now() - startTime)
+        if (response.headers['x-response-time']) {
+            serverElapsed.read.push(Number(response.headers['x-response-time']))
+        }
         assert.strictEqual(response.data.id, id)
         assert.strictEqual(response.data.title, `${content.title} (updated)`)
         assertRecentDate(new Date(response.data.updated_at))
 
         // DELETE
         startTime = Date.now()
-        await axios.delete(`${BASE_URL}/content/${id}`)
+        response = await deleteContent(id, { headers })
         requestElapsed.delete.push(Date.now() - startTime)
+        if (response.headers['x-response-time']) {
+            serverElapsed.delete.push(Number(response.headers['x-response-time']))
+        }
 
         // READ
-        startTime = Date.now()
-        await axios.get(`${BASE_URL}/content/${id}`, { validateStatus: (status) => status === 404 })
+        startTime = Date.now()        
+        response = await getContent(id, { headers })
+        if (TEST_SUPABASE) {
+            assert.strictEqual(response.data, undefined)
+        } else {
+            assert.strictEqual(response.status, 404)
+        }
         requestElapsed.read.push(Date.now() - startTime)
+        if (response.headers['x-response-time']) {
+            serverElapsed.read.push(Number(response.headers['x-response-time']))
+        }
     }
 
     return {
         testElapsed: Date.now() - testStartTime,
+        serverElapsed,
         requestElapsed,
         requestCount: Object.values(requestElapsed).flat().length,
     }
@@ -145,7 +206,18 @@ async function runBatch(batchIndex, batch) {
             const result = await runTest(batchIndex, index);
             return result;
         } catch (error) {
-            logError(`Error thrown running test`, { batchIndex, index, error: error.stack || error })
+            logError(`Error thrown running test`, {
+                batchIndex,
+                index,
+                error: error.stack || error,
+                response: { status: error.response?.status, data: error.response?.data },
+                request: {
+                    url: error.config?.url,
+                    method: error.config?.method,
+                    headers: error.config?.headers,
+                    data: error.config?.data,
+                },
+            })
             throw error;
         }
     }))
@@ -175,6 +247,7 @@ async function main() {
     const updateElapsed = successResults.map(result => result.value.requestElapsed.update).flat()
     const deleteElapsed = successResults.map(result => result.value.requestElapsed.delete).flat()
     const requestElapsed = successResults.map(result => Object.values(result.value.requestElapsed).flat()).flat()
+    const serverElapsed = successResults.map(result => Object.values(result.value.serverElapsed).flat()).flat()
     logInfo(`Finished performance test`, {
         TEST_LIMIT,
         TEST_PARALLEL,
@@ -190,6 +263,7 @@ async function main() {
         updateElapsed: stats(updateElapsed),
         deleteElapsed: stats(deleteElapsed),
         requestElapsed: stats(requestElapsed),
+        serverElapsed: stats(serverElapsed),
         requests: {
             totalCount: requestCount,
             countPerSecond: requestCount / (elapsedTotal / 1000),
